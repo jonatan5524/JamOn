779813613f2c09b182619a1aa1dd13c24064f14b
noamguy <noamguy8@gmail.com>
2026-05-19 16:28:10 +0300

    Added event participation

--- apps/orchestrator/src/modules/event/dto/join-event.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class JoinEventDto {
-  @ApiProperty({ example: 'user_123456' })
-  userId!: string;
-}
--- apps/orchestrator/src/modules/event/event.entity.ts
+++ apps/orchestrator/src/modules/event/event.entity.ts
@@ -1,10 +1,15 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn, Index, OneToMany } from 'typeorm';
 import { User } from '../user/user.entity';
+import { EventParticipant } from './event-participant.entity';
 
 @Entity('events')
 export class Event {
     @PrimaryGeneratedColumn({ type: 'bigint' })
     id!: string;
+
+    @Index({ unique: true })
+    @Column({ type: 'varchar', length: 6 })
+    code!: string;
 
     @Column({ type: 'text' })
     title!: string;
@@ -18,4 +23,7 @@ export class Event {
     @ManyToOne(() => User)
     @JoinColumn({ name: 'created_by' })
     creator!: User;
+
+    @OneToMany(() => EventParticipant, (p) => p.event)
+    participants!: EventParticipant[];
 }
